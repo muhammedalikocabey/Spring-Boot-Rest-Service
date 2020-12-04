3b35ba6e2a5c12eb33de124e8516b7a7b1d1e758
muhammedalikocabey <muhammedkocabey98@hotmail.com>
2020-12-04 03:54:53 +0300

    Case 1: XMLParser has been added for daily Parity data and run when the REST Service is started
    type : feat
    
    - XMLParser Class has been defined to save parity data to the database when the service is started.
    - The daily Parity data in XML format taken from the site https://www.tcmb.gov.tr/kurlar/today.xml was parsed.
    - To save the parsed log data when the service is started: SpringApplication.run method is overridden and parseAndSaveParityData method is called inside the run method.

--- src/main/java/com/makocabey/rest/SpringRestServiceApplication.java
+++ src/main/java/com/makocabey/rest/SpringRestServiceApplication.java
@@ -2,21 +2,34 @@ package com.makocabey.rest;
 
 
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
 @SpringBootApplication
-public class SpringRestServiceApplication {
+public class SpringRestServiceApplication implements CommandLineRunner {
 
+	private XMLService xmlService;
+	
+	public SpringRestServiceApplication(XMLService xmlService) {
+		this.xmlService = xmlService;
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		SpringApplication app = new SpringApplication(SpringRestServiceApplication.class);
 		
 		app.setBannerMode(Banner.Mode.OFF);
 		app.run(args);
-		
 	}
-
+	
+	
+	
+	@Override
+	public void run(String...args) throws Exception {
+		xmlService.parseAndSaveParityData();
+	}
 }
---
+++ src/main/java/com/makocabey/rest/XMLService.java
@@ -0,0 +1,95 @@
+package com.makocabey.rest;
+
+
+import org.springframework.stereotype.Service;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+@Service
+public class XMLService {
+
+	private final Logger log = LoggerFactory.getLogger(XMLService.class);
+
+	
+	private final ParityRepository repository;
+	
+	public XMLService(ParityRepository repository) {
+		this.repository = repository; 
+	}
+	
+	
+	
+	public void parseAndSaveParityData() { 
+		
+		
+		try {
+			String URL = "https://www.tcmb.gov.tr/kurlar/today.xml";
+					
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			
+			Document doc = builder.parse(URL);
+			
+			
+			doc.getDocumentElement().normalize();
+			
+			
+			String sDate = doc.getElementsByTagName("Tarih_Date").item(0).getAttributes().getNamedItem("Date").getNodeValue();
+			
+			
+			DateTimeFormatter format = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+			
+			LocalDate date = LocalDate.parse(sDate, format);
+			
+			
+			NodeList parityNodeList = doc.getElementsByTagName("Currency");
+			
+			
+			for(int i = 0; i < parityNodeList.getLength(); i++) {
+				Node node = parityNodeList.item(i);
+				
+				if(node.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) node;
+					
+					String parityCode = (elem.getAttribute("CurrencyCode") + "TRY");
+					Integer unit = Integer.parseInt(elem.getElementsByTagName("Unit").item(0).getTextContent());
+					Double purchasePrice = Double.parseDouble(elem.getElementsByTagName("ForexBuying").item(0).getTextContent());
+					purchasePrice /= unit;
+					Double salePrice = Double.parseDouble(elem.getElementsByTagName("ForexSelling").item(0).getTextContent());
+					salePrice /= unit;
+					Double averagePrice = (purchasePrice + salePrice) / 2;
+					
+					
+					Parity parity = new Parity(parityCode, date, purchasePrice, salePrice, averagePrice);
+					
+					
+					repository.save(parity);
+					
+				}
+				
+			}
+			
+			
+		}
+		
+		catch (Exception e) {
+			log.error(e.getMessage());
+		}
+	}
+	
+}
